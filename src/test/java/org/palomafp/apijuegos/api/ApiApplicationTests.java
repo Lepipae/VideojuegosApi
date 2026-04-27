b4595fa9ba4fd915ffcf4b4145e1a2dc93b2c66c
Andrés López <delaviandres@gmail.com>
2026-04-27 12:09:09 +0200

    Inicializar proyecto maven springboot

---
+++ src/test/java/org/palomafp/apijuegos/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.palomafp.apijuegos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
