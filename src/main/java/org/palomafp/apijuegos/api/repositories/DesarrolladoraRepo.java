6e848f803e477fa14647ae7953dbd46f6e6c2644
Andrés López <delaviandres@gmail.com>
2026-05-06 17:59:27 +0200

    feature: añadidos metodos de busqueda

--- src/main/java/org/palomafp/apijuegos/api/repositories/DesarrolladoraRepo.java
+++ src/main/java/org/palomafp/apijuegos/api/repositories/DesarrolladoraRepo.java
@@ -4,5 +4,6 @@ import org.palomafp.apijuegos.api.modelo.Desarrolladora;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface DesarrolladoraRepo extends MongoRepository<Desarrolladora, String> {
-
+    Desarrolladora findByMiId(int miId);
+    Desarrolladora findByNombre(String nombre);
 }
