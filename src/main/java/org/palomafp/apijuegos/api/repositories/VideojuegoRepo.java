f46bf9dfff0512ab3d52dcfda97cd61c41ccc3cd
Andrés López <delaviandres@gmail.com>
2026-05-06 17:59:31 +0200

    feature: añadidos metodos de busqueda

--- src/main/java/org/palomafp/apijuegos/api/repositories/VideojuegoRepo.java
+++ src/main/java/org/palomafp/apijuegos/api/repositories/VideojuegoRepo.java
@@ -4,9 +4,13 @@ import org.palomafp.apijuegos.api.modelo.Videojuego;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VideojuegoRepo extends MongoRepository<Videojuego, String> {
     Videojuego findByMiId(long miId);
     Videojuego findByNombre(String nombre);
+    List<Videojuego> findByTags(String tags);
+    List<Videojuego> findByIdDesarrolladora(int idDesarrolladora);
     void deleteByMiId(long miId);
 }
