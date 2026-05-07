0e77f468bbdd838b77ba11f23a4864317a66cec5
Andrés López <delaviandres@gmail.com>
2026-05-07 10:09:25 +0200

    feature: añadidos metodos al controlador de entradalista y añadir buscar por id interno en EntradaListaRepo

--- src/main/java/org/palomafp/apijuegos/api/controladores/EntradaListaController.java
+++ src/main/java/org/palomafp/apijuegos/api/controladores/EntradaListaController.java
@@ -1,4 +1,30 @@
 package org.palomafp.apijuegos.api.controladores;
 
+import org.palomafp.apijuegos.api.modelo.EntradaLista;
+import org.palomafp.apijuegos.api.repositories.EntradaListaRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/lista")
 public class EntradaListaController {
+    @Autowired
+    private EntradaListaRepo entradaListaRepo;
+
+    @GetMapping("/{miId}")
+    public List<EntradaLista> findByIdUsuario(@PathVariable("miId") int id) {
+        return entradaListaRepo.findByIdUsuario(id);
+    }
+
+    @GetMapping("/id/{id}")
+    public EntradaLista findById(@PathVariable("id") long id) {
+        return entradaListaRepo.findByMiId(id);
+    }
+
+
 }
--- src/main/java/org/palomafp/apijuegos/api/repositories/EntradaListaRepo.java
+++ src/main/java/org/palomafp/apijuegos/api/repositories/EntradaListaRepo.java
@@ -3,6 +3,10 @@ package org.palomafp.apijuegos.api.repositories;
 import org.palomafp.apijuegos.api.modelo.EntradaLista;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface EntradaListaRepo extends MongoRepository<EntradaLista, String> {
-    EntradaLista findByIdUsuario(long id);
+    List<EntradaLista> findByIdUsuario(int id);
+
+    EntradaLista findByMiId(long id);
 }
